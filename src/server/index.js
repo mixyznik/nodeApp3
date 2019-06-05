2e20712f24ede5729d286b722ff3f4cfb51f8aef
mixyznik <mixy80@gmail.com>
2019-06-05 16:19:25 +0200

    Jenkinsfile 232s

--- src/server/index.js
+++ src/server/index.js
@@ -4,6 +4,6 @@ const os = require('os');
 const app = express();
 
 app.use(express.static('dist'));
-app.get('/api/getUsername', (req, res) => res.send({ username: "Mixy-radi node" }));
+app.get('/api/getUsername', (req, res) => res.send({ username: "Mixy-radi node-najnovije" }));
 
 app.listen(process.env.PORT || 9090, () => console.log(`Listening on port ${process.env.PORT || 9090}!`));
